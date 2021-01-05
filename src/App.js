130c455cee7525da5e52065a3160b915f8f17554
razhnoff <shahteryshka@gmail.com>
2021-01-06 02:11:48 +0300

    init commit

--- src/App.js
+++ src/App.js
@@ -2,13 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const exchangeRateApi = 'https://api.exchangeratesapi.io/latest'
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
